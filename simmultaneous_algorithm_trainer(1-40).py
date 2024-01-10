7fff152fb46f8284491d09274e68fae8b82c170a
JacksonRichards <55330240+JacksonRichards@users.noreply.github.com>
2024-01-09 19:21:44 -0800

    Add files via upload

---
+++ simmultaneous_algorithm_trainer(1-40).py
@@ -0,0 +1,225 @@
+
+
+import os
+import threading
+
+
+print("THREADING TRAINING PROCCESS COMMENCING")
+
+
+def execute_script(name):
+    os.system('python ' + name)
+
+
+
+a = threading.Thread(target=execute_script, args=('algo_backtester_1_V1_28APR23.py',))
+a.start()
+#print(a)
+
+b = threading.Thread(target=execute_script, args=('algo_backtester_2_V1_28APR23.py',))
+b.start()
+#print(b)
+
+c = threading.Thread(target=execute_script, args=('algo_backtester_3_V1_28APR23.py',))
+c.start()
+#print(c)
+
+d = threading.Thread(target=execute_script, args=('algo_backtester_4_V1_28APR23.py',))
+d.start()
+#print(d)
+
+e = threading.Thread(target=execute_script, args=('algo_backtester_5_V1_28APR23.py',))
+e.start()
+#print(e)
+
+f = threading.Thread(target=execute_script, args=('algo_backtester_6_V1_28APR23.py',))
+f.start()
+#print(f)
+
+g = threading.Thread(target=execute_script, args=('algo_backtester_7_V1_28APR23.py',))
+g.start()
+#print(g)
+
+h = threading.Thread(target=execute_script, args=('algo_backtester_8_V1_28APR23.py',))
+h.start()
+#print(h)
+
+i = threading.Thread(target=execute_script, args=('algo_backtester_9_V1_28APR23.py',))
+i.start()
+#print(i)
+
+j = threading.Thread(target=execute_script, args=('algo_backtester_10_V1_28APR23.py',))
+j.start()
+#print(j)
+
+k = threading.Thread(target=execute_script, args=('algo_backtester_11_V1_28APR23.py',))
+k.start()
+#print(k)
+
+l = threading.Thread(target=execute_script, args=('algo_backtester_12_V1_28APR23.py',))
+l.start()
+#print(l)
+
+m = threading.Thread(target=execute_script, args=('algo_backtester_13_V1_28APR23.py',))
+m.start()
+#print(m)
+
+n = threading.Thread(target=execute_script, args=('algo_backtester_14_V1_28APR23.py',))
+n.start()
+#print(n)
+
+o = threading.Thread(target=execute_script, args=('algo_backtester_15_V1_28APR23.py',))
+o.start()
+#print(o)
+
+p = threading.Thread(target=execute_script, args=('algo_backtester_16_V1_28APR23.py',))
+p.start()
+#print(p)
+
+q = threading.Thread(target=execute_script, args=('algo_backtester_17_V1_28APR23.py',))
+q.start()
+#print(q)
+
+r = threading.Thread(target=execute_script, args=('algo_backtester_18_V1_28APR23.py',))
+r.start()
+#print(r)
+
+s = threading.Thread(target=execute_script, args=('algo_backtester_19_V1_28APR23.py',))
+s.start()
+#print(s)
+
+t = threading.Thread(target=execute_script, args=('algo_backtester_20_V1_28APR23.py',))
+t.start()
+#print(t)
+
+u = threading.Thread(target=execute_script, args=('algo_backtester_21_V1_28APR23.py',))
+u.start()
+#print(u)
+
+v = threading.Thread(target=execute_script, args=('algo_backtester_22_V1_28APR23.py',))
+v.start()
+#print(v)
+
+w = threading.Thread(target=execute_script, args=('algo_backtester_23_V1_28APR23.py',))
+w.start()
+#print(w)
+
+x = threading.Thread(target=execute_script, args=('algo_backtester_24_V1_28APR23.py',))
+x.start()
+#print(x)
+
+y = threading.Thread(target=execute_script, args=('algo_backtester_25_V1_28APR23.py',))
+y.start()
+#print(y)
+
+z = threading.Thread(target=execute_script, args=('algo_backtester_26_V1_28APR23.py',))
+z.start()
+#print(z)
+
+aa = threading.Thread(target=execute_script, args=('algo_backtester_27_V1_28APR23.py',))
+aa.start()
+#print(aa)
+
+bb = threading.Thread(target=execute_script, args=('algo_backtester_28_V1_28APR23.py',))
+bb.start()
+#print(bb)
+
+cc = threading.Thread(target=execute_script, args=('algo_backtester_29_V1_28APR23.py',))
+cc.start()
+#print(cc)
+
+dd = threading.Thread(target=execute_script, args=('algo_backtester_30_V1_28APR23.py',))
+dd.start()
+#print(dd)
+
+ee = threading.Thread(target=execute_script, args=('algo_backtester_31_V1_28APR23.py',))
+ee.start()
+#print(ee)
+
+ff = threading.Thread(target=execute_script, args=('algo_backtester_32_V1_28APR23.py',))
+ff.start()
+#print(ff)
+
+gg = threading.Thread(target=execute_script, args=('algo_backtester_33_V1_28APR23.py',))
+gg.start()
+#print(gg)
+
+hh = threading.Thread(target=execute_script, args=('algo_backtester_34_V1_28APR23.py',))
+hh.start()
+#print(hh)
+
+ii = threading.Thread(target=execute_script, args=('algo_backtester_35_V1_28APR23.py',))
+ii.start()
+#print(ii)
+
+jj = threading.Thread(target=execute_script, args=('algo_backtester_36_V1_28APR23.py',))
+jj.start()
+#print(jj)
+
+kk = threading.Thread(target=execute_script, args=('algo_backtester_37_V1_28APR23.py',))
+kk.start()
+#print(kk)
+
+ll = threading.Thread(target=execute_script, args=('algo_backtester_38_V1_28APR23.py',))
+ll.start()
+#print(ll)
+
+mm = threading.Thread(target=execute_script, args=('algo_backtester_39_V1_28APR23.py',))
+mm.start()
+#print(mm)
+
+nn = threading.Thread(target=execute_script, args=('algo_backtester_40_V1_28APR23.py',))
+nn.start()
+#print(nn)
+
+
+
+a.join()
+b.join()
+c.join()
+d.join()
+e.join()
+f.join()
+g.join()
+h.join()
+i.join()
+j.join()
+k.join()
+l.join()
+m.join()
+n.join()
+o.join()
+p.join()
+q.join()
+r.join()
+s.join()
+t.join()
+u.join()
+v.join()
+w.join()
+x.join()
+y.join()
+z.join()
+aa.join()
+bb.join()
+cc.join()
+dd.join()
+ee.join()
+ff.join()
+gg.join()
+hh.join()
+ii.join()
+jj.join()
+kk.join()
+ll.join()
+mm.join()
+nn.join()
+
+
+
+
+print("THREADING TRAINING PROCCESS COMPLETE")
+
+
+
+
